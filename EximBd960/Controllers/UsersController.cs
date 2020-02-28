80dffcaf62ba56e6156a174830acf08b8c3fb35c
himuahmed <himuh12@gmail.com>
2020-02-28 23:22:12 +0600

    ##Added permission based log in and sign up (Need to fix some bug)

---
+++ EximBd960/Controllers/UsersController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Configuration;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Web.UI.WebControls;
+using EximBd960.Models;
+
+namespace EximBd960.Controllers
+{
+
+    public class UsersController : Controller
+    {
+        
+        private String connectionString = WebConfigurationManager.ConnectionStrings["Eximbd960DbContext"].ConnectionString;
+        
+        public ActionResult LogIn()
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogIn(User user)
+        {
+            bool result = Loggedin(user);
+            if (result)
+            {
+                FormsAuthentication.SetAuthCookie(user.UserName, false);
+                return RedirectToAction("Index", "Applicants");
+            }
+            ModelState.AddModelError("", "Invalid username or password");
+            return View();
+
+        }
+
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(User model)
+        {
+            bool result = AddUser(model);
+
+            if (result)
+            {
+                return RedirectToAction("LogIn", "Users");
+            }
+            else
+            {
+                return RedirectToAction("SignUp", "Users");
+            }
+
+            //return View();
+        }
+
+        public bool AddUser(User user)
+        {
+            
+            SqlConnection connection = new SqlConnection(connectionString);
+            string query = "INSERT INTO Users (UserName,Password) VALUES (@Username,@Password)";
+            SqlCommand cmd = new SqlCommand(query, connection);
+
+            cmd.Parameters.Add("Username", SqlDbType.VarChar);
+            cmd.Parameters["Username"].Value = user.UserName;
+
+            cmd.Parameters.Add("Password", SqlDbType.VarChar);
+            cmd.Parameters["Password"].Value = user.Password;
+            connection.Open();
+            int isSaved = cmd.ExecuteNonQuery();
+            connection.Close();
+            if (isSaved > 0)
+            {
+                ModelState.Clear();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Loggedin(User user)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            string query = "SELECT * FROM Users WHERE (UserName = @Username  AND Password = @Password)";
+            SqlCommand cmd = new SqlCommand(query, connection);
+
+            cmd.Parameters.Add("Username", SqlDbType.VarChar);
+            cmd.Parameters["Username"].Value = user.UserName;
+
+            cmd.Parameters.Add("Password", SqlDbType.VarChar);
+            cmd.Parameters["Password"].Value = user.Password;
+
+            connection.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.Read() == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("LogIn", "Users");
+        }
+    }
+}
---
+++ EximBd960/Models/RoleManager.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Security;
+
+namespace EximBd960.Models
+{
+    public class RoleManager : RoleProvider
+    {
+        private String connectionString = WebConfigurationManager.ConnectionStrings["Eximbd960DbContext"].ConnectionString;
+        public override string ApplicationName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        private string[] User = new string[1];
+        private string Role;
+        public override string[] GetRolesForUser(string username)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            string query = "SELECT Role FROM Users WHERE (UserName = '" + username + "')";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            connection.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Role = reader["Role"].ToString();
+                User[0] = Role;
+            }
+            return User;
+            //throw new NotImplementedException();
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsUserInRole(string username, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
